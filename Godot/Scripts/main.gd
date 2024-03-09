f03cb9aef6edc282821b256b17fd8dc0e8602e23
Wyatt5150 <wyatt.mahoney5150@gmail.com>
2024-03-08 23:43:59 -0600

    Some Changes

---
+++ Godot/Scripts/main.gd
@@ -0,0 +1,18 @@
+extends Node
+
+var current_level = "title"
+var level_path = r"res://Scenes/Levels/"
+@onready var level_container = $Level
+@onready var shade = $Shade
+@onready var shade_text = $Shade/Text
+
+
+func _ready():
+	Load_Level("title", "Loading...")
+
+
+func Load_Level(toLoad : String, message = "SLIME!"):
+	current_level = toLoad
+	shade_text.text = message
+	shade.show()
+	print(toLoad)
