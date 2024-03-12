9ae26caa11397f22e81df1ffaee49c227c854f0e
OrusLiOx <y.lily.n@gmail.com>
2024-03-12 04:33:09 -0500

    water dash level

--- Godot/Player/Player.gd
+++ Godot/Player/Player.gd
@@ -109,8 +109,8 @@ func swim_process(delta):
 	if !in_water():
 		return
 	extraJump=true
+	var direction = Input.get_axis("Up", "Down")
 	if parasite == "Dive":
-		var direction = Input.get_axis("Up", "Down")
 		if direction:
 			# holding up or down
 			if !jumping:
@@ -136,7 +136,7 @@ func swim_process(delta):
 			velocity.y *= 2/3.0
 	else:
 		if !on_surface():
-			velocity.y = gravity/-20
+			velocity.y = gravity/-20 + direction * 40
 		elif !jumping:
 			velocity.y =0
 	velocity.x *= 2/3.0
